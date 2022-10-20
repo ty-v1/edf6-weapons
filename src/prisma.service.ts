c62512b717537c40856c06a86ce9b7b612bf94b8
ty-v1 <fmtyd509@gmail.com>
2022-10-20 17:58:48 +0900

    enable sql logging

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,34 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel> implements OnModuleInit {
+
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({ log: ['query', 'info', 'warn', 'error'] });
+  }
+
   async onModuleInit() {
+    this.$on('query', (event) => {
+      this.logger.log(
+        `Query: ${event.query}`,
+        `Params: ${event.params}`,
+        `Duration: ${event.duration} ms`,
+      );
+    });
+    this.$on('info', (event) => {
+      this.logger.log(`message: ${event.message}`);
+    });
+    this.$on('error', (event) => {
+      this.logger.log(`error: ${event.message}`);
+    });
+    this.$on('warn', (event) => {
+      this.logger.log(`warn: ${event.message}`);
+    });
+
+
     await this.$connect();
   }
 

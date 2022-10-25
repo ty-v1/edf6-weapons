7ea216e737d8a0d6bfa64f9c5c9cba9abf123c85
ty-v1 <fmtyd509@gmail.com>
2022-10-25 12:57:41 +0900

    add env variable

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,20 @@ import { AppModule } from './app.module';
 import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+const resolveStaticAssetsDir = (isDev: boolean) => {
+  return isDev ? path.join(__dirname, '..', '..', 'public') : path.join(__dirname, '..', 'public');
+};
+
+const resolveBaseViewsDir = (isDev: boolean) => {
+  return isDev ? path.join(__dirname, '..', '..', 'views') : path.join(__dirname, '..', 'views');
+};
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(path.join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
+  const isDev = process.env.NODE_ENV != 'prd';
+  app.useStaticAssets(resolveStaticAssetsDir(isDev));
+  app.setBaseViewsDir(resolveBaseViewsDir(isDev));
   app.setViewEngine('hbs');
 
   await app.listen(3000);

8aef36578df3673c5768a1a8a75c163ce9c2a810
ty-v1 <fmtyd509@gmail.com>
2023-08-09 17:05:02 +0900

    add aws resource clients

---
+++ app/server/drop/createDynamoDbClient.ts
@@ -0,0 +1,18 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+
+export const dynamoDbClient = (() => {
+  if (process.env.NODE_ENV !== 'production') {
+    return new DynamoDBClient({
+      region: 'us-east-1',
+      endpoint: 'http://localhost:4545',
+      credentials: {
+        accessKeyId: 'dummy',
+        secretAccessKey: 'dummy',
+      },
+    });
+  } else {
+    return new DynamoDBClient({
+      region: 'ap-northeast-1',
+    });
+  }
+})();
---
+++ app/server/weapon/createS3Client.ts
@@ -0,0 +1,19 @@
+import { S3Client } from '@aws-sdk/client-s3';
+
+export const s3Client = (() => {
+  if (process.env.NODE_ENV !== 'production') {
+    return new S3Client({
+      region: 'ap-northeast-1',
+      endpoint: 'http://localhost:3090',
+      credentials: {
+        accessKeyId: 'minio',
+        secretAccessKey: 'password',
+      },
+      forcePathStyle: true,
+    });
+  } else {
+    return new S3Client({
+      region: 'ap-northeast-1',
+    });
+  }
+})();

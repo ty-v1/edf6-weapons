8d808cf3f0e07bf7526586ded09a1c97f5eaae75
ty-v1 <fmtyd509@gmail.com>
2023-08-12 18:18:15 +0900

    fix

--- app/server/weapon/listWeapons.ts
+++ app/server/weapon/listWeapons.ts
@@ -2,8 +2,17 @@ import { WeaponCategory } from '~/server/weapon/WeaponCategory';
 import { sendS3SelectQuery } from '~/server/weapon/sendS3SelectQuery';
 
 export const listWeapons = async (category: WeaponCategory) => {
-  return await sendS3SelectQuery(`SELECT *
+  const weapons = await sendS3SelectQuery(`SELECT *
                                   FROM S3Object s3
-                                  WHERE s3.category = '${category}'
-                                  ORDER BY s3.level ASC`);
+                                  WHERE s3.category = '${category}'`);
+
+  weapons.sort((a, b) => {
+    if (a.level !== b.level) {
+      return a.level - b.level;
+    }
+
+    return a.name.localeCompare(b.name);
+  });
+
+  return weapons;
 };

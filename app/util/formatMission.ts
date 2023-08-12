a6a4c28850cba4620d05f835d6043d5766dca8f6
ty-v1 <fmtyd509@gmail.com>
2023-08-12 18:18:04 +0900

    fix

--- app/util/formatMission.ts
+++ app/util/formatMission.ts
@@ -6,13 +6,13 @@
  * @example [168, 208]: DLC2 1-40
  */
 export const formatMission = (mission: number) => {
+  if (mission <= 147) {
+    return mission.toString();
+  }
+
   if (mission <= 167) {
     return `DLC1 ${mission - 147}`;
   }
 
-  if (mission <= 208) {
-    return `DLC2 ${mission - 167}`;
-  }
-
-  return mission.toString();
+  return `DLC2 ${mission - 167}`;
 };
